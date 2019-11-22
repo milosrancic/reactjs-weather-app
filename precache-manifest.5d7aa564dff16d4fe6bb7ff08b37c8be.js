871595f89873bd4da7b79338e8131406344cb5c1
Milos <milosrancic94@gmail.com>
2019-11-22 19:07:18 +0100

    Updates

---
+++ precache-manifest.5d7aa564dff16d4fe6bb7ff08b37c8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef098b4e5205dd2dd45d0419b41c2bf",
+    "url": "/reactjs-weather-app/index.html"
+  },
+  {
+    "revision": "5b35791a3e1be1eead48",
+    "url": "/reactjs-weather-app/static/css/main.b631b6fd.chunk.css"
+  },
+  {
+    "revision": "04f81830a31d30226db5",
+    "url": "/reactjs-weather-app/static/js/2.0f5d5ddf.chunk.js"
+  },
+  {
+    "revision": "5b35791a3e1be1eead48",
+    "url": "/reactjs-weather-app/static/js/main.c8efd536.chunk.js"
+  },
+  {
+    "revision": "22f5820c2a48f447c68a",
+    "url": "/reactjs-weather-app/static/js/runtime-main.9c6aca62.js"
+  },
+  {
+    "revision": "df7f14ca567d908108706180c3d58405",
+    "url": "/reactjs-weather-app/static/media/background.df7f14ca.jpg"
+  }
+]);
